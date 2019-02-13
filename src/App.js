8c5696efdad5609cb6c6c63e205fef9b80477629
root <nikeshp96@gmail.com>
2019-02-13 00:39:47 -0800

    using map to make tic-tac-toe board, trying to figure out how to bind onClick properly

--- src/App.js
+++ src/App.js
@@ -3,40 +3,75 @@ import logo from './logo.svg';
 import './App.css';
 
 
-let tictactoe = {
-  board : [0,0,0,0,0,0,0,0,0],
-  id : 0,
-}
+const tictactoe = [
+  {
+    index:0,
+    isSet:""      
+  },
+  {
+    index:1,
+    isSet:""      
+  },  {
+    index:2,
+    isSet:""      
+  },  {
+    index:3,
+    isSet:""      
+  },  {
+    index:4,
+    isSet:""      
+  },  {
+    index:5,
+    isSet:""      
+  },  {
+    index:6,
+    isSet:""      
+  },  {
+    index:7,
+    isSet:""      
+  },  {
+    index:8,
+    isSet:""      
+  }
+]
+
 
 
 class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = tictactoe;
-    this.onDismiss =  this.onDismiss.bind(this);
+    this.state = {
+      list:tictactoe,
+    }
+    this.onDismiss = this.onDismiss.bind(this);
   }
+
+  onDismiss(e){
+    console.log(e);
+    const isNotId = item => item.index !== e;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({list:updatedList});
+  }
+
   render() {
     return (
       <div className="App">
         <div className="grid">
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
+          {this.state.list.map(function(item){
+            return (
+              <button className="cell" onClick={()=>this.onDismiss(item.index)} type="button" >{item.isSet}</button>
+            )
+
+          })}
         </div>
       </div>
     );
   }
 
-  onDismiss(id){
 
-    }
+
+
 
 }
 
